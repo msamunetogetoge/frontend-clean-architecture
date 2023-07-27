1add3cf9b51a37695d1b68913c154a8796d6b3ef
masamune <kenjiii534@gmail.com>
2023-07-27 22:30:58 +0900

    ok

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -6,6 +6,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 1 + 1 to equal 3', () => {
-    expect(sum(1, 1)).toBe(3);
-});
+// test('adds 1 + 1 to equal 3', () => {
+//     expect(sum(1, 1)).toBe(3);
+// });
